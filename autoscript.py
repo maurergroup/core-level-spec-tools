7ee49f267a735f87700b26a13f2388b596f7cafd
Sam Hall <mstrdw@godzilla.csc.warwick.ac.uk>
2020-03-26 11:01:52 +0000

    Updates NEXAFS scripts and added 2 postprocessing scripts

--- autoscript.py
+++ autoscript.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python
 
+import os
+import shutil
 from ase.calculators.castep import Castep
 from ase.io import read
 from core_excitation import CoreExcitation
 from core_excitation import NEXAFS, XPS
 
+input_name = 'azulene'
+output_name = 'azulene_gas'
+
 QM1 = Castep(
-            castep_command='/storage/molases/mstrdw/CASTEP_bins/SCRTP_castep_bin/castep.mpi',	#Directory path to location of castep binary
+            castep_command='/storage/molases/mstrdw/CASTEP_bins/SCRTP_castep_bin/castep19/castep.mpi',	#Directory path to location of castep binary
             castep_pp_path='/home/molases/mstrdw/data/code/pps',				#Directory path to folder of pseudopotential
-            label='unit',									#Name of castep input files
+            label=output_name,			#Seedname of the output files
             _rename_existing_dir=False,							
-            xc_functional='PBE',								#List of general parameters for the .param file
+            xc_functional='PBE',	#List of general parameters for the .param file
             spin_polarized=False,
             data_distribution='default',
-            elec_energy_tol='1e-08',
+            elec_energy_tol='1e-07',
             grid_scale=2.0,
             iprint=1.0,
             max_scf_cycles=300,
             metals_method='dm',
             mixing_scheme='Pulay',
-            nextra_bands=100,
+            nextra_bands=800,
+            elnes_nextra_bands=800,
             smearing_scheme='Gaussian',
             smearing_width=0.1,
             fix_occupancy=False,
@@ -29,19 +35,33 @@ QM1 = Castep(
             pdos_calculate_weights=True,
             fix_com=False,
             fix_all_cell=True,
-            kpoints_mp_grid='8 8 1',
+            kpoints_mp_grid='1 1 1',
             kpoints_mp_offset='0. 0. 0.',
             reuse=True,
             _export_settings=False,
             _pedantic=False,
-            _find_pspots=True)
+            _find_pspots=False)
 
-QM1.param.cut_off_energy = 350		#Input cut-off energy
+QM1.param.cut_off_energy = 450		#Input cut-off energy
 
-cell = read('geometrynext.in');		#Change to required input file
+cell = read(input_name+'.cell');		#Change to required input file
 ce = XPS(atoms=cell, element='C', pspots='2|1.4|10|12|13|20:21{1s1,2s2,2p3}(qc=7)', calc=QM1)	#Change to required species and custom pseudopotential needed 
 ce.move_hole()
 
-cell = read('geometrynext.in');		#Change to required input file
+cell = read(input_name+'.cell');		#Change to required input file
 ce = NEXAFS(atoms=cell, element='C', pspots='2|1.4|10|12|13|20:21{1s1.5,2s2,2p2.5}(qc=7)', calc=QM1)	#Change to require species and custom pseudopotential needed
 ce.move_hole()
+
+source = 'pps/'
+dest1 = 'XPS/'
+dest2 = 'NEXAFS/'
+
+#files = os.listdir(source)
+#direcs = os.listdir(dest1)
+#
+#for x in direcs:
+#    for f in files:
+#        shutil.copy2(source+f, dest1+x)
+#        shutil.copy2(source+f, dest2+x)
+
+
---
+++ plot_mo.py
@@ -0,0 +1,107 @@
+import numpy as np
+
+def gaussian(x, x_mean, broadening):
+
+    gaussian_val = np.sqrt((4*np.log(2))/(np.pi*(broadening**2)))* np.exp(-((4*np.log(2))/(broadening**2))*(x-x_mean)**2);
+    return gaussian_val
+
+def lorentzian(x, x_mean, broadening):
+
+    lorentzian_val = (1/(2*np.pi))* (broadening)/(((broadening/2)**2)+(x-x_mean)**2);
+    return lorentzian_val
+
+def PseudoVoigt(x, x_mean, broadening, mixing):
+    """ 
+    Combines gaussian and lorentzian schemes together
+    """
+    return (1-mixing)*gaussian(x, x_mean, broadening)+mixing*lorentzian(x, x_mean, broadening)
+
+def dos_binning(eigenvalues,broadening=0.75, bin_width=0.01, mix1=0., mix2 = None,
+        coeffs=None,start=0.0, stop=10.0, broadening2 = None, ewid1 = 10.0, ewid2 = 20.0):
+    """ 
+    performs binning for a given set of eigenvalues and 
+    optionally weight coeffs.
+    """
+    if broadening2 is None:
+        broadening2 = broadening
+    if coeffs is None:
+        coeffs = np.ones(len(eigenvalues))
+    lowest_e = start
+    highest_e = stop
+    num_bins = int((highest_e-lowest_e)/bin_width)
+    x_axis = np.zeros([num_bins])
+    data = np.zeros([num_bins])
+    #setting up x-axis
+    for i in range(num_bins):
+        x_axis[i] = lowest_e + i * bin_width
+    #get DOS
+    sigma=np.zeros((len(eigenvalues)))
+    mixing=np.zeros((len(eigenvalues)))
+
+    for ei,e in enumerate(eigenvalues):
+        if e<=(ewid1):
+            sigma[ei]=broadening
+            mixing[ei]=mix1
+        elif e>(ewid2):
+            sigma[ei]=broadening2
+            mixing[ei]=mix2
+        else:
+            sigma[ei]=broadening + ((broadening2-broadening)/(ewid2-ewid1))*(e-ewid1)
+            mixing[ei]=(mix1 + ((mix2-mix1)/(ewid2-ewid1))*(e-ewid1))
+    for i in range(num_bins):
+        pseudovoigt_vec = np.zeros((len(eigenvalues)))
+        pseudovoigt_vec=PseudoVoigt(x_axis[i],eigenvalues,sigma,mixing)*coeffs
+        data[i]= np.sum(pseudovoigt_vec)
+    return x_axis, data
+
+######################################
+#Broadening Parameters
+xstart = 285.
+xstop = 310.
+broad1 = 0.75
+broad2 = 2.0
+firstpeak = 290.0 
+ewid1 = firstpeak+5.0
+ewid2 = firstpeak+15.0
+mix1 = 0.2
+mix2 = 0.8
+########################################
+
+#File Names
+MO = ['37','38','39','40','41','42','43'] #List of MO numbers
+angle = ['t25','t53','t90'] #incidence angles
+molecule = 'benzene'
+metal = 'Cu'
+filename = '/'+molecule+'_'+metal+'_4_1_1_1_deltas.dat'
+
+
+peaks = np.zeros([6,38048]) #Empty array of No.of carbons, No. of states
+I = np.zeros([6,38048])
+
+for m in MO:
+        
+    for a in angle:
+        for i,direc in enumerate(['C1/','C2/','C3/','C4/','C5/','C6/']):
+            
+            data = np.loadtxt(direc+a+filename)
+            x, y = data[:,0], data[:,3]
+            
+            data2 = np.loadtxt(direc+a+'/'+molecule+'_'+m+'_spin1_deltas.dat')
+            peaks[i,:] = x
+            I[i,:] = y*data2[:,1]
+        
+        fileout = open(molecule+'_'+metal+'_MO'+m+'_deltas_'+a+'.txt','w')
+        fileout.write('#   <x in eV>     Intensity\n')
+        for p,i in zip(peaks.flatten(), I.flatten()):
+            fileout.write('{0:16.8f}    {1:16.8f}\n'.format(p,i))
+        fileout.close()
+            
+        x, y = dos_binning(peaks.flatten(), broadening=broad1, mix1=mix1, mix2=mix2, start=xstart, stop=xstop,
+                coeffs = I.flatten(), broadening2=broad2, ewid1=ewid1, ewid2=ewid2)
+        
+        datafile = open(molecule+'_'+metal+'_MO'+m+'_'+a+'.txt', 'w')
+        for (xi, yi) in zip(x,y):
+            asd = str(xi) + ' ' + str(yi) + '\n'
+            datafile.write(asd)
+        datafile.close()
+
