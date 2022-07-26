2be7796937f5b2388d80416155d10167fc1ba351
Sam Hall <sam.hall.5@warwick.ac.uk>
2022-07-26 12:38:48 +0100

    Remove obsolete broadening parameters

--- plot_xps.py
+++ plot_xps.py
@@ -1,5 +1,56 @@
 import numpy as np
 
+######BROADENING PARAMETERS###################################
+
+xstart = 285. #Start Value
+xstop = 305. #End Value
+broad = 0.7 #Broadening value for first section 
+mix = 0.3 #G/L mix ratio
+
+##############################################################
+
+atom_contribute = False
+
+def main():
+#Set what element you have calculated XPS for
+    element = 'C'
+#Read in the XPS peaks in generated with python script
+    xps_data = np.loadtxt(element+'_XPS_peaks.txt')
+    print(xps_data)
+
+#Apply the broadening
+    x, y = dos_binning(xps_data, broadening=broad, mix=mix, start=xstart, stop=xstop,
+                coeffs = None)
+
+#Write out the spectrum to a text file
+    spec_file = open(element+'_XPS_spectrum.txt', 'w')
+    for (xi, yi) in zip(x,y):
+        spec_dat = str(xi) + ' ' + str(yi) + '\n'
+        spec_file.write(spec_dat)
+    spec_file.close()
+
+    if atom_contribute == True:
+        xs = []
+        ys = []
+
+        for z in range(len(xps_data)):
+            peak = []
+            peak.append(xps_data[z])
+            x_tmp, y_tmp = dos_binning(peak, broadening=broad, mix=mix, start=xstart, stop=xstop,
+                        coeffs = None,)
+            xs.append(x_tmp)
+            ys.append(y_tmp)
+
+            atom_file = open(element+'_XPS_spectrum_'+element+str(z)+'.txt','w')
+            for (xsz, ysz) in zip(x_tmp, y_tmp):
+                atom_data = str(xsz) + ' ' + str(ysz) + '\n'
+                atom_file.write(atom_data)
+            atom_file.close()
+        else:
+            quit()
+
+##############################################################
+
 def gaussian(x, x_mean, broadening):
     
     gaussian_val = np.sqrt((4*np.log(2))/(np.pi*(broadening**2)))* np.exp(-((4*np.log(2))/(broadening**2))*(x-x_mean)**2);
@@ -16,14 +67,12 @@ def PseudoVoigt(x, x_mean, broadening, mixing):
     """
     return (1-mixing)*gaussian(x, x_mean, broadening)+mixing*lorentzian(x, x_mean, broadening)
 
-def dos_binning(eigenvalues,broadening=0.75, bin_width=0.01, mix1=0., mix2 = None,
-        coeffs=None,start=0.0, stop=10.0, broadening2 = None, ewid1 = 10.0, ewid2 = 20.0):
+def dos_binning(eigenvalues, broadening=0.75, bin_width=0.01, mix=0.,
+        coeffs=None, start=0.0, stop=10.0):
     """ 
     performs binning for a given set of eigenvalues and 
     optionally weight coeffs.
     """
-    if broadening2 is None:
-        broadening2 = broadening
     if coeffs is None:
         coeffs = np.ones(len(eigenvalues))
     lowest_e = start
@@ -35,70 +84,13 @@ def dos_binning(eigenvalues,broadening=0.75, bin_width=0.01, mix1=0., mix2 = Non
     for i in range(num_bins):
         x_axis[i] = lowest_e + i * bin_width
     #get DOS
-    sigma=np.zeros((len(eigenvalues)))
-    mixing=np.zeros((len(eigenvalues)))
+    sigma=broadening
+    mixing=mix
 
-    for ei,e in enumerate(eigenvalues):
-        if e<=(ewid1):
-            sigma[ei]=broadening
-            mixing[ei]=mix1
-        elif e>(ewid2):
-            sigma[ei]=broadening2
-            mixing[ei]=mix2
-        else:
-            sigma[ei]=broadening + ((broadening2-broadening)/(ewid2-ewid1))*(e-ewid1)
-            mixing[ei]=(mix1 + ((mix2-mix1)/(ewid2-ewid1))*(e-ewid1))
     for i in range(num_bins):
         pseudovoigt_vec = np.zeros((len(eigenvalues)))
         pseudovoigt_vec=PseudoVoigt(x_axis[i],eigenvalues,sigma,mixing)*coeffs
         data[i]= np.sum(pseudovoigt_vec)
     return x_axis, data
 
-###################################
-xstart = 285.
-xstop = 305.
-broad1 = 0.7 
-broad2 = 0.7 
-firstpeak = 285.0
-ewid1 = firstpeak+1.0
-ewid2 = firstpeak+2.0
-mix1 = 0.3
-mix2 = 0.3
-########################################
-
-#Set what element you have calculated XPS for
-element = 'C'
-#Read in the XPS peaks in generated with python script
-data = np.loadtxt(element+'_XPS_peaks.txt')
-print(data)
-
-#Apply the broadening
-x, y = dos_binning(data, broadening=broad1, mix1=mix1, mix2=mix2, start=xstart, stop=xstop,
-                coeffs = None, broadening2=broad2, ewid1=ewid1, ewid2=ewid2)
-
-#Write out the spectrum to a text file
-fileout = open(element+'_XPS_spectrum.txt', 'w')
-for (xi, yi) in zip(x,y):
-    dat = str(xi) + ' ' + str(yi) + '\n'
-    fileout.write(dat)
-fileout.close()
-
-#To get the indivdual atom peaks uncomment the quit() command
-quit()
-
-xs = []
-ys = []
-
-for z in range(len(data)):
-    peak = []
-    peak.append(data[z])
-    x_tmp, y_tmp = dos_binning(peak, broadening=broad1, mix1=mix1, mix2=mix2, start=xstart, stop=xstop,
-                coeffs = None, broadening2=broad2, ewid1=ewid1, ewid2=ewid2)
-    xs.append(x_tmp)
-    ys.append(y_tmp)
-
-    txtfile = open(element+'_XPS_spectrum_'+element+str(z)+'.txt','w')
-    for (xsz, ysz) in zip(x_tmp, y_tmp):
-        txt = str(xsz) + ' ' + str(ysz) + '\n'
-        txtfile.write(txt)
-    txtfile.close()
+main()
